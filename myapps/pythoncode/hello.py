aca97c9f964fe8b24787681636b68420987fb27f
Ashutoshh <asashuthegreat4@gmail.com>
2021-05-25 15:38:41 +0530

    Add files via upload

---
+++ myapps/pythoncode/hello.py
@@ -0,0 +1,12 @@
+import time
+import  os
+x1=os.environ['x']
+
+while int(x1) > 101 :
+    print("Hello all , welcome to python..!!")
+    time.sleep(3)
+    print("Welcome to LnB..")
+    time.sleep(2)
+    print("Welcome to Containers ..!!")
+    print("______________________")
+    time.sleep(3)
